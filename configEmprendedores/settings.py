1e80bc5af59ad259b06c38ffae68409ca8598397
andre salvador <salvadorclesmer@gmail.com>
2026-05-10 10:08:37 -0600

    feat / actualizacion del cors y requirements

--- configEmprendedores/settings.py
+++ configEmprendedores/settings.py
@@ -66,6 +66,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:8000',
+    'http://localhost:5713',
+    'http://127.0.0.1:5713',
 ]
 
 ROOT_URLCONF = 'configEmprendedores.urls'
